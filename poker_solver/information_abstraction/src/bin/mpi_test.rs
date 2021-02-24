f196975a9350fb16d6341b65911f4e102c9c5fdb
Kyle Murphy <kwmurph2@asu.edu>
2021-02-24 14:26:18 -0700

    mpi test

--- poker_solver/information_abstraction/src/bin/mpi_test.rs
+++ poker_solver/information_abstraction/src/bin/mpi_test.rs
@@ -3,5 +3,15 @@ fn main() {
     let world = universe.world();
     let size = world.size();
     let rank = world.rank();
-    println!("rank: {}, size: {}", rank, size);
+    let root_process = world.process_at_rank(0);
+
+    let indexes = (0..169).iter().collect();
+    let mut batch = Vec::new();
+    if rank == 0 {
+        root_process.scatter_into_root(&indexes, &mut batch);
+    } else {
+        root_process.scatter_into(&mut batch);
+    }
+
+    println!("rank: {}, batch: {:?}", rank, batch);
 }
