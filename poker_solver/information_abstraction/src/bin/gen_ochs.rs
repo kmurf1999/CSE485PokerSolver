7ab0ecdce368f6164f5a6dd782826967f40ab086
Kyle Murphy <kwmurph2@asu.edu>
2021-02-25 22:18:09 -0700

    gen ochs

--- poker_solver/information_abstraction/src/bin/gen_ochs.rs
+++ poker_solver/information_abstraction/src/bin/gen_ochs.rs
@@ -1,7 +1,6 @@
 use clap::Clap;
 
 use information_abstraction::ochs::gen_ochs_features;
-use rust_poker::read_write::VecIO;
 use std::error::Error;
 use std::result::Result;
 
@@ -10,13 +9,10 @@ use std::result::Result;
 struct Opts {
     #[clap(short, long, default_value = "3")]
     round: u8,
-    #[clap(short, long, default_value = "1000")]
-    sim_count: u64,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opts: Opts = Opts::parse();
-    assert!(opts.sim_count > 0);
     assert!(opts.round > 1 && opts.round < 4);
 
     gen_ochs_features(opts.round)?;
