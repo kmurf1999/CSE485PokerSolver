1ecf4a656f57377315e7acee538d6fa5aef8e873
Kyle Murphy <kwmurph2@asu.edu>
2021-03-24 17:22:34 -0700

    avg -> current

--- poker_solver/src/solver.rs
+++ poker_solver/src/solver.rs
@@ -381,7 +381,7 @@ impl<'a> SolverThread<'a> {
                     return node_util;
                 }
                 let strategy =
-                    self.root.infosets[*index as usize].current_strategy(bucket, n_actions);
+                    self.root.infosets[*index as usize].cummulative_strategy(bucket, n_actions);
                 // let strategy =
                 // self.root.infosets[*index as usize].cummulative_strategy(bucket, n_actions);
                 let sampled_action = sample_pdf(&strategy, &mut self.rng);
